f4e1aa00f3fff453a2d2432565b55e3124990dd1
Giorgos Brantzos <g.brantzos@outlook.com>
2023-08-29 21:26:06 +0300

    Add class to component

--- libs/ui-blocks/src/lib/ui-blocks.component.ts
+++ libs/ui-blocks/src/lib/ui-blocks.component.ts
@@ -1,18 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'ubl-ui-blocks',
   standalone: true,
   imports: [CommonModule],
-  template: `
-    <p>
-      ui-blocks works! YEAH!
-    </p>
-  `,
-  styles: [
-  ]
+  template: ` <p>ui-blocks works! YEAH!</p> `,
+  styles: [],
 })
 export class UiBlocksComponent {
-
+  // https://stackoverflow.com/questions/34641281/how-to-add-class-to-host-element
+  @HostBinding('class') classes = 'someClass1';
 }
