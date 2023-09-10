ed4b3ad890ecc5855e2044d27f5b367bc68210c2
Giorgos Brantzos <g.brantzos@outlook.com>
2023-09-10 11:18:34 +0300

    Introduce Angular Material

--- apps/oms/src/app/app.component.ts
+++ apps/oms/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { Component, inject } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { UiBlocksComponent } from 'ui-blocks';
 import { RouterListenerService } from './core/services/router-listener.service';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'oms-root',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterOutlet, UiBlocksComponent],
+  imports: [CommonModule, RouterLink, RouterOutlet, UiBlocksComponent, MatProgressBarModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
